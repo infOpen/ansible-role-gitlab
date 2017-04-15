0b09d5c976cff4aa1cf46548dd4356938e3a9683
Alexandre Chaussier <a.chaussier@infopen.pro>
2017-04-15 17:58:44 +0200

    Manage system prerequisites on Debian and Ubuntu

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -2,14 +2,24 @@
 Role tests
 """
 
+import pytest
 from testinfra.utils.ansible_runner import AnsibleRunner
 
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_foo_a(User):
-    assert User().name == 'root'
+def test_system_prerequisites(Package, SystemInfo):
+    """
+    Check system prerequisites packages installed
+    """
 
+    packages = []
 
-def test_foo_b(User):
-    assert User().name == 'root'
+    if SystemInfo.distribution.lower() in ['debian', 'ubuntu']:
+        packages = ['apt-transport-https', 'ca-certificates', 'curl',
+                    'debian-archive-keyring', 'openssh-server', 'postfix']
+    else:
+        pytest.skip('Not apply to %s' % SystemInfo.distribution)
+
+    for package in packages:
+        assert Package(package).is_installed
